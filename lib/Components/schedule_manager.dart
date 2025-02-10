71c4d89ad77861922cd6c20323d0ff287d8f89bf
Charlie Brewis <charliebrewis7@gmail.com>
2025-02-10 13:21:55 +0000

    Moved IGUI to be part of IScheduleManager

--- lib/Components/schedule_manager.dart
+++ lib/Components/schedule_manager.dart
@@ -3,8 +3,15 @@
 class Task { /* Stub implementation of Task */ }
 
 // Interface for ScheduleManager to interact with the GUI
-// I.e., ScheduleManager -> GUI methods
 abstract class IScheduleManager {
+  // ScheduleManager -> GUI methods
   Future<List<Task>> getSchedule(DateTime date);
   Future<List<Task>> getBacklogSuggestions();
+
+  // GUI -> ScheduleManager methods
+  Future<void> addTask(Task task);
+  Future<void> deleteTask(int taskId);
+  Future<void> editTask(Task task);
+  Future<void> postPoneTask(int taskId);
+  Future<List<Task>> scheduleBacklogSuggestion(int taskId);
 }
