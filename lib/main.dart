e9a31982353f5968d6e194af6f5a754487b0b261
Charlie Brewis <charliebrewis7@gmail.com>
2025-03-25 15:57:58 +0000

    Deleted unused imports from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:student_75/app_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:student_75/userInterfaces/category_ranking.dart';
-import 'package:student_75/models/task_model.dart';
-import 'package:student_75/Components/schedule_manager/schedule.dart';
 
 Future<void> main() async {
   // Ensure that the Flutter binding is initialized before calling any plugins
   WidgetsFlutterBinding.ensureInitialized();
 
   // Load the app settings
-  // await AppSettings.loadSettings();
+  await AppSettings.loadSettings();
 
   runApp(
     const MyApp(),
