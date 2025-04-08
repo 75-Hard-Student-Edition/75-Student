c7d397cd3dab9a3ecf38195ceb85fed052349714
WIDALI <widadoalli@gmail.com>
2025-04-08 15:54:06 +0100

    Improvements to Settings Page

--- lib/userInterfaces/settings_page.dart
+++ lib/userInterfaces/settings_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:student_75/userInterfaces/start_up.dart';
+import 'package:student_75/userInterfaces/home.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -9,8 +12,8 @@ class SettingsPage extends StatelessWidget {
     double topPadding = MediaQuery.of(context).padding.top;
 
     return Scaffold(
-      backgroundColor: const Color(0xE0E6F4F3),
-      bottomNavigationBar: CustomBottomNavBar(),
+      backgroundColor: const Color(0xFFE6F4F3),
+      bottomNavigationBar: const CustomBottomNavBar(),
       body: Padding(
         padding: EdgeInsets.only(top: topPadding + 10, left: 16, right: 16),
         child: Column(
@@ -19,17 +22,26 @@ class SettingsPage extends StatelessWidget {
             Row(
               children: [
                 IconButton(
-                  icon: const Icon(Icons.arrow_back_ios_new_rounded),
+                  icon: const Icon(Icons.arrow_back, size: 30),
                   onPressed: () => Navigator.pop(context),
                   color: Colors.black87,
                 ),
-                const SizedBox(width: 8),
-                Text(
-                  "Settings",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.teal[700],
+              ],
+            ),
+            const SizedBox(height: 10),
+            const Row(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      "Settings",
+                      style: TextStyle(
+                        fontFamily: 'kdamThmorPro',
+                        fontSize: 45,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF00B3A1),
+                      ),
+                    ),
                   ),
                 ),
               ],
@@ -38,7 +50,7 @@ class SettingsPage extends StatelessWidget {
             Container(
               padding: const EdgeInsets.symmetric(horizontal: 16),
               decoration: BoxDecoration(
-                color: Colors.white,
+                color: const Color(0xFFEBEFF0),
                 borderRadius: BorderRadius.circular(12),
                 boxShadow: const [
                   BoxShadow(
@@ -49,7 +61,8 @@ class SettingsPage extends StatelessWidget {
               ),
               child: const TextField(
                 decoration: InputDecoration(
-                  hintText: "search...",
+                  hintText: "Search...",
+                  hintStyle: TextStyle(color: Color(0xFFAFA9A9)),
                   border: InputBorder.none,
                 ),
               ),
@@ -64,11 +77,45 @@ class SettingsPage extends StatelessWidget {
             const SizedBox(height: 16),
             Center(
               child: TextButton.icon(
-                onPressed: () {},
+                onPressed: () {
+                  showCupertinoDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return CupertinoAlertDialog(
+                        title: const Text("Log Out"),
+                        content: const Text("Are you sure you want to log out?"),
+                        actions: [
+                          CupertinoDialogAction(
+                            child: const Text("Cancel"),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                          CupertinoDialogAction(
+                            isDestructiveAction: true,
+                            child: const Text("Yes"),
+                            onPressed: () {
+                              Navigator.pop(context);
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(builder: (context) => const WelcomeScreen()),
+                              );
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
                 icon: const Icon(Icons.block, color: Colors.red),
                 label: const Text(
                   "Log Out",
-                  style: TextStyle(color: Colors.red, fontSize: 16),
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'kdamThmorPro',
+                  ),
                 ),
               ),
             )
@@ -82,21 +129,35 @@ class SettingsPage extends StatelessWidget {
     return Column(
       children: [
         ListTile(
-          leading: Icon(icon, color: Colors.black54),
+          leading: Icon(
+            icon,
+            size: 30,
+            color: Colors.white,
+            shadows: const [
+              Shadow(
+                offset: Offset(0, 4),
+                blurRadius: 8,
+                color: Color(0x3D000000),
+              ),
+            ],
+          ),
           title: Text(
             title,
             style: const TextStyle(
-              fontSize: 18,
-              color: Colors.black87,
-              fontWeight: FontWeight.w500,
+              fontFamily: 'kdamThmorPro',
+              fontSize: 35,
+              color: Color(0xFF787878),
+              fontWeight: FontWeight.w400,
             ),
           ),
-          trailing: const Icon(Icons.chevron_right),
           onTap: () {
             print('$title tapped');
           },
         ),
-        const Divider(thickness: 1, height: 0),
+        const Padding(
+          padding: EdgeInsets.only(top: 5),
+          child: Divider(thickness: 1, height: 0),
+        ),
       ],
     );
   }
@@ -107,24 +168,61 @@ class CustomBottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-      color: Colors.teal,
-      shape: const CircularNotchedRectangle(),
-      notchMargin: 6,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          IconButton(
-              icon: const Icon(Icons.home, color: Colors.white), onPressed: () {}),
-          IconButton(
-              icon: const Icon(Icons.person, color: Colors.white), onPressed: () {}),
-          const SizedBox(width: 40), // Space for center button
-          IconButton(
-              icon: const Icon(Icons.add, color: Colors.white), onPressed: () {}),
-          IconButton(
-              icon: const Icon(Icons.blur_on, color: Colors.white), onPressed: () {}),
-        ],
-      ),
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        BottomAppBar(
+          color: const Color(0xFF00B3A1),
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 6,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.home, color: Colors.white, size: 25),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const ScheduleScreen()),
+                  );
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.person, color: Colors.white, size: 25),
+                onPressed: () {},
+              ),
+              const SizedBox(width: 40), // space taken by FAB
+              IconButton(
+                icon: const Icon(Icons.add, color: Colors.white, size: 25),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const ScheduleScreen()),
+                  );
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.blur_on, color: Colors.white, size: 25),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 50,
+          child: FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.white,
+            elevation: 4,
+            shape: const CircleBorder(),
+            child: const Icon(
+              Icons.settings,
+              color: Color(0xFF00B3A1),
+              size: 40,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
