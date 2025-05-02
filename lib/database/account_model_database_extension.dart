6613486080c81e08737015af32927146c0778527
Thomas <thomasisafisher@google.com>
2025-05-02 14:40:42 +0100

    Wrote fromMap for userAccountModel

--- lib/database/account_model_database_extension.dart
+++ lib/database/account_model_database_extension.dart
@@ -3,11 +3,11 @@ import 'package:student_75/models/user_account_model.dart';
 import 'package:student_75/models/difficulty_enum.dart';
 
 extension UserAccountModelDB on UserAccountModel {
-  String _categoryOrderToString(List<TaskCategory> categoryOrder) {
+  static String _categoryOrderToString(List<TaskCategory> categoryOrder) {
     return categoryOrder.map((e) => e.toString().split(".").last).join(",");
   }
 
-  List<TaskCategory> _categoryOrderFromString(String categoryOrderStr) {
+  static List<TaskCategory> _categoryOrderFromString(String categoryOrderStr) {
     return categoryOrderStr
         .split(",")
         .map((e) => TaskCategory.values
@@ -26,18 +26,23 @@ extension UserAccountModelDB on UserAccountModel {
       "category_order": _categoryOrderToString(categoryOrder!),
       "sleep_duration_minutes": sleepDuration!.inMinutes,
       "bedtime": bedtime!.toIso8601String(),
-      "notify_time_minutes": bedtimeNotifyBefore,
+      "notify_time_minutes": bedtimeNotifyBefore!.inMinutes,
       "mindfulness_minutes": mindfulnessDuration!.inMinutes,
     };
   }
 
   static UserAccountModel fromMap(Map<String, dynamic> map) {
     return UserAccountModel(
-      id: map["user_id"],
-      username: map["username"],
-      email: map["email"] == "" ? null : map["email"],
-      phoneNumber: map["phone_number"] == "" ? null : map["phone_number"],
-      streak: map["streak"],
-    );
+        id: map["user_id"],
+        username: map["username"],
+        email: map["email"] == "" ? null : map["email"],
+        phoneNumber: map["phone_number"] == "" ? null : map["phone_number"],
+        streak: map["streak"],
+        difficulty: Difficulty.values[map["difficulty"]],
+        categoryOrder: _categoryOrderFromString(map["category_order"]),
+        sleepDuration: Duration(minutes: map["sleep_duration_minutes"]),
+        bedtime: DateTime.parse(map["bedtime"]),
+        bedtimeNotifyBefore: Duration(minutes: map["notify_time_minutes"]),
+        mindfulnessDuration: Duration(minutes: map["mindfulness_minutes"]));
   }
 }
