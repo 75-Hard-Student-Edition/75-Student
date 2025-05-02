54492446a8d144316ffca1beb6eabdb2f3c42d05
Thomas <thomasisafisher@google.com>
2025-05-02 14:23:58 +0100

    Added to and from strings helper function for categoryOrder

--- lib/database/account_model_database_extension.dart
+++ lib/database/account_model_database_extension.dart
@@ -1,7 +1,20 @@
+import 'package:student_75/models/task_model.dart';
 import 'package:student_75/models/user_account_model.dart';
 import 'package:student_75/models/difficulty_enum.dart';
 
 extension UserAccountModelDB on UserAccountModel {
+  String _categoryOrderToString(List<TaskCategory> categoryOrder) {
+    return categoryOrder.map((e) => e.toString().split(".").last).join(",");
+  }
+
+  List<TaskCategory> _categoryOrderFromString(String categoryOrderStr) {
+    return categoryOrderStr
+        .split(",")
+        .map((e) => TaskCategory.values
+            .firstWhere((item) => item.toString().split(".").last == e))
+        .toList();
+  }
+
   Map<String, dynamic> toMap() {
     return {
       "user_id": id,
@@ -10,7 +23,7 @@ extension UserAccountModelDB on UserAccountModel {
       "email": email ?? "",
       "phone_number": phoneNumber ?? "",
       "difficulty": Difficulty.values.indexOf(difficulty!),
-      "category_order": categoryOrder.toString(),
+      "category_order": _categoryOrderToString(categoryOrder!),
       "sleep_duration_minutes": sleepDuration!.inMinutes,
       "bedtime": bedtime!.toIso8601String(),
       "notify_time_minutes": bedtimeNotifyBefore,
