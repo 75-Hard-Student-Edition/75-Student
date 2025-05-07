06c9510dc05b4098be65fb33f6f000da98463481
Charlie Brewis <charliebrewis7@gmail.com>
2025-05-07 15:03:03 +0100

    Added comment

--- lib/userInterfaces/home.dart
+++ lib/userInterfaces/home.dart
@@ -428,7 +428,7 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
                     scheduleManager.editTask(overlappingTask.copyWith(endTime: _tentativeStart!));
                   }
                   _fetchDisplaySchedule();
-                  _buildProgressBar(context, pointsManager);
+                  _buildProgressBar(context, pointsManager); // Update progress bar on deletion
                 }
 
                 setState(() {
